3cf11cf02ef124876ca41e4cec0bce3a0cb36542
FBence123 <FBence123@users.noreply.github.com>
2019-12-17 14:18:50 +0100

    Updated main script

--- homeo_RL.py
+++ homeo_RL.py
@@ -6,6 +6,7 @@ Created on Sun Nov 24 13:12:22 2019
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patch
 import scipy.stats as scista
 import numpy as np
 
@@ -29,6 +30,20 @@ temp = np.zeros((N,49,A))
 p_tol = np.zeros((N,A))
 c_l = np.zeros((N,A))
 
+def ethanol (norm):
+    x = np.arange(1/47,1,1/47)
+    y = scista.beta.pdf(x = x, a = 1.35, b = 6)
+    y = list(np.concatenate((np.zeros(3), y)))
+    y = [-(((y[j]-min(y))*norm)/max(y)-min(y)) for j in range(len(y))]
+    return y
+
+def tolerance (norm):
+    x = np.arange(1/50,1,1/50)
+    y = list(scista.beta.pdf(x = x, a = 2.2, b = 6))
+    y = [((y[j]-min(y))*norm)/max(y)-min(y) for j in range(len(y))]
+    y[0] = 0
+    return y
+
 for a in range(A):
     Q_tol_a = 0
     Q_nul_a = 23
@@ -39,18 +54,12 @@ for a in range(A):
         c = int(np.random.rand() < p_tol[t,a])
     
         # Ethanol injection
-        x = np.arange(1/47,1,1/47)
-        y = scista.beta.pdf(x = x, a = 1.35, b = 6)
-        y = list(np.concatenate((np.zeros(3), y)))
-        y = [((y[j]-min(y))*14)/max(y)-min(y) for j in range(len(y))]
-        temp[t,:,a] = [temp[t,j,a] - y[j] for j in range(temp.shape[1])]
+        y = ethanol(14)
+        temp[t,:,a] = [temp[t,j,a] + y[j] for j in range(temp.shape[1])]
         
         # Tolerance response if chosen
         if c == 1:
-           x = np.arange(1/50,1,1/50)
-           y = list(scista.beta.pdf(x = x, a = 2.2, b = 6))
-           y = [((y[j]-min(y))*12)/max(y)-min(y) for j in range(len(y))]
-           y[0] = 0
+           y = tolerance(12)
            temp[t,:,a] = [temp[t,j,a] + y[j] for j in range(temp.shape[1])]
             
         # Calculate drives and rewards
@@ -66,9 +75,59 @@ for a in range(A):
         Q_nul[t+1,a] = Q_nul_a
         c_l[t,a] = c
 
+#Ethanol injection plot (Figure 3B)
+y = ethanol(1.4)
+plt.figure()
+plt.plot(np.arange(0,49),y,"k-")
+plt.hlines(0,0,49,colors = "silver", linestyles = "dashed")
+plt.vlines([2,3,4,5,6],-1.5,1.5,colors = ["red","silver","silver","silver","silver"])
+plt.annotate("Ethanol injection",xy=(2,0.5),xytext=(12,1), arrowprops=dict(facecolor='red', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.xticks(np.arange(0,49,6),np.arange(0,25,3))
+plt.ylim([-1.5,1.5])
+plt.xlabel("Hour")
+plt.ylabel("Change of temperature")
+plt.title("Ethanol injection")
 
+#Tolerance response plot (Figure 3C)
+y = tolerance(1.2)
 plt.figure()
-temp_plot = temp.mean(2)
-plt.plot(temp_plot.mean(1))
+plt.plot(np.arange(0,49),y,"k-")
+plt.hlines(0,0,49,colors = "silver", linestyles = "dashed")
+plt.vlines([0,3,4,5,6],-1.5,1.5,colors = ["green","silver","silver","silver","silver"])
+plt.annotate("Tolerance response",xy=(0,-0.5),xytext=(12,-1), arrowprops=dict(facecolor='green', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.xticks(np.arange(0,49,6),np.arange(0,25,3))
+plt.ylim([-1.5,1.5])
+plt.xlabel("Hour")
+plt.ylabel("Change of temperature")
+plt.title("Tolerance response")
+
+#Tolerance response + ethanol injection plot (Figure 3F)
+y = ethanol(1.4)
+y2 = tolerance(1.2)
+y = [y[i] + y2[i] for i in range(len(y))]
 plt.figure()
-plt.plot(p_tol.mean(1))
+plt.plot(np.arange(0,49),y,"k-")
+plt.hlines(0,0,49,colors = "silver", linestyles = "dashed")
+plt.vlines([0,2,3,4,5,6],-1.5,1.5,colors = ["green","red","silver","silver","silver","silver"])
+plt.annotate("Ethanol injection",xy=(2,-0.7),xytext=(12,-0.7), arrowprops=dict(facecolor='red', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.annotate("Tolerance response",xy=(0,-1),xytext=(12,-1), arrowprops=dict(facecolor='green', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.annotate("30'",xy=(3,1.3),xytext=(15,1.3), arrowprops=dict(facecolor='silver', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.annotate("60'",xy=(4,1.1),xytext=(15,1.1), arrowprops=dict(facecolor='silver', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.annotate("90'",xy=(5,0.9),xytext=(15,0.9), arrowprops=dict(facecolor='silver', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.annotate("120'",xy=(6,0.7),xytext=(15,0.7), arrowprops=dict(facecolor='silver', shrink=0.05, connectionstyle="angle3,angleA=0,angleB=90"))
+plt.xticks(np.arange(0,49,6),np.arange(0,25,3))
+plt.ylim([-1.5,1.5])
+plt.xlabel("Hour")
+plt.ylabel("Change of temperature")
+plt.title("Tolerance response + Ethanol injection")
+
+#Response probability plot (Figure 3C)
+plt.figure()
+plt.plot(np.arange(1,11),p_tol.mean(1),"k-")
+plt.plot(np.arange(1,11),p_tol.mean(1),"ko")
+plt.ylim([0,1])
+plt.xticks(np.arange(1,11),["1","2","3","4","5","6","7","8","E1","R1"])
+plt.gca().add_patch(patch.Polygon([[8.5,0],[9.5,0],[9.5,1],[8.5,1]],color = "silver"))
+plt.xlabel("Blocks")
+plt.ylabel("Response probability")
+
